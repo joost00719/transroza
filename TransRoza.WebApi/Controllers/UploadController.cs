6c4fdf16657ae4f3d0dafe235477c03e7a2c4e12
Joost Roza <joost@roza.dev>
2023-08-22 12:56:14 +0200

    Progress

---
+++ TransRoza.WebApi/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TransRoza.Shared;
+
+namespace TransRoza.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost(Name = "CreateHandle")]
+        public Guid CreateFileHandle(RozaFileInformation fileInfo)
+        {
+            fileInfo.Handle = Guid.NewGuid();
+
+            return fileInfo.Handle.Value;
+        }
+
+        [HttpPatch(Name = "UploadChunk/{handle}/{beginInclusive}/{endInclusive}")]
+        public async Task<IActionResult> UploadChunk([FromQuery] Guid handle, [FromQuery] int beginInclusive, [FromBody] Stream data)
+        {
+            Directory.CreateDirectory("/data/uploads");
+
+            using (var fs = System.IO.File.OpenWrite($@"/data/uploads/{handle}.txt"))
+            {
+                fs.Seek(beginInclusive, SeekOrigin.Begin);
+                await data.CopyToAsync(fs);
+                await fs.FlushAsync();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ TransRoza.WebApi/ModelBinders/Stream/StreamBinder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TransRoza.WebApi.ModelBinders.Stream
+{
+    public class StreamBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.HttpContext.Request.Body);
+            return Task.CompletedTask;
+        }
+    }
+}
